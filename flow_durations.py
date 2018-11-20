82c2cdbde2d951dc13639cd670d15a027ab27add
David Wan <dave.wan@mail.utoronto.ca>
2018-11-20 15:14:11 -0500

    Reorganized code

--- flow_durations.py
+++ flow_durations.py
@@ -1,62 +1,20 @@
 from scapy.all import *
 import csv
-# from course_project.count_flows import *
-PATH = "/home/radiantwings/Documents/univ1_trace/univ1_pt3"
+from util import *
 
-def get_total_duration(flow):
-    duration = 0
-    for i in range(1, len(flow)):
-        duration += flow[i].time - flow[i-1].time
-    return duration
 
 if __name__ == "__main__":
-    pcap = PcapReader(PATH)
+    fuck = process_pcap()
+    tcpflows = fuck[0]
+    udpflows = fuck[1]
 
     output = open("tcp_flow_durations.csv", "w")
     tcp_writer = csv.writer(output, delimiter=',',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
     udp_output = open("udp_flow_durations.csv", "w")
-    udp_writer = csv.writer(output, delimiter=',',
+    udp_writer = csv.writer(udp_output, delimiter=',',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
-
-    tcpflows = {}
-    udpflows = {}
-    i = 0
-
-    for p in pcap:
-        # print(i)
-        if IP in p:
-            if TCP in p:
-                a_to_b = p[IP].src + ":" + str(p[TCP].sport) + " -> " + p[IP].dst + ":" + str(p[TCP].dport)
-                b_to_a = p[IP].dst + ":" + str(p[TCP].dport) + " -> " + p[IP].src + ":" + str(p[TCP].sport)
-                # if a_to_b in flows or b_to_a in flows:
-                #     flows[a_to_b].append(p)
-                # else:
-                if a_to_b not in tcpflows and b_to_a not in tcpflows:
-                    tcpflows[a_to_b] = []
-                if a_to_b in tcpflows:
-                    tcpflows[a_to_b].append(p.time)
-                elif b_to_a in tcpflows:
-                    tcpflows[b_to_a].append(p.time)
-            elif UDP in p:
-                a_to_b = p[IP].src + ":" + str(p[UDP].sport) + " -> " + p[IP].dst + ":" + str(p[UDP].dport)
-                b_to_a = p[IP].dst + ":" + str(p[UDP].dport) + " -> " + p[IP].src + ":" + str(p[UDP].sport)
-                # if a_to_b in flows or b_to_a in flows:
-                #     flows[a_to_b].append(p)
-                # else:
-                if a_to_b not in udpflows and b_to_a not in udpflows:
-                    udpflows[a_to_b] = []
-                if a_to_b in udpflows:
-                    udpflows[a_to_b].append(p.time)
-                elif b_to_a in udpflows:
-                    udpflows[b_to_a].append(p.time)
-            # if (i > 200):
-            #
-            #     for k, v in flows.items():
-            #         print(k, v)
-            #     quit(1)
-            i += 1
 
     for k, v in tcpflows.items():
         # print(k, v)
@@ -64,7 +22,7 @@ if __name__ == "__main__":
 
     print("============================UDP FLOWS============================")
     for k, v in udpflows.items():
-        # print(k, v)
+        print(k, v)
         udp_writer.writerow([k, v[len(v) - 1] - v[0]])
 
     output.close()
