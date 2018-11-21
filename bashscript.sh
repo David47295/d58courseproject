148a0f8f4162408cda3f45c1000e2c98d40d4985
David Wan <dave.wan@mail.utoronto.ca>
2018-11-21 14:18:29 -0500

    Fixed measuring duration

--- bashscript.sh
+++ bashscript.sh
@@ -2,10 +2,10 @@
 
 echo "Generating univ1_pt3_tcp.csv"
 
-tshark -r univ1_pt3 -n -T fields -e ip.src -e tcp.srcport -e ip.dst -e tcp.dstport -e tcp.stream -Y tcp -E header=y -E occurrence=f -E separator=, > univ1_pt3_tcp.csv
+tshark -r univ1_pt3 -n -T fields -e ip.src -e tcp.srcport -e ip.dst -e tcp.dstport -e tcp.stream -e frame.time -Y tcp -E header=y -E occurrence=f -E separator=, > univ1_pt3_tcp.csv
 
 echo "Generating univ1_pt3_udp.csv"
 
-tshark -r univ1_pt3 -n -T fields -e ip.src -e udp.srcport -e ip.dst -e udp.dstport -e udp.stream -Y udp -E header=y -E occurrence=f -E separator=, > univ1_pt3_udp.csv
+tshark -r univ1_pt3 -n -T fields -e frame.number -e ip.src -e udp.srcport -e ip.dst -e udp.dstport -e udp.stream -e frame.time_epoch -Y udp -E header=y -E occurrence=f -E separator=, > univ1_pt3_udp.csv
 
 
--- main.py
+++ main.py
@@ -10,11 +10,8 @@ if __name__ == "__main__":
     # tcp_reader = csv.reader(tcp_file, delimiter=',', quotechar='|')
     tcp_reader = csv.DictReader(tcp_file)
 
-    # pcap = PcapReader(PATH)
-
     tcpflows = {}
     udpflows = {}
-    i = 0
 
     for row in tcp_reader:
 
@@ -25,21 +22,39 @@ if __name__ == "__main__":
         is_inverse_recorded = b_to_a in tcpflows
 
         if not is_recorded and not is_inverse_recorded:
-            tcpflows[a_to_b] = []
-        if is_recorded:
+            tcpflows[a_to_b] = [row]
+        elif is_recorded:
             tcpflows[a_to_b].append(row)
         elif is_inverse_recorded:
             tcpflows[b_to_a].append(row)
 
-        if (i > 200):
-            for k, v in tcpflows.items():
-                print(k,v)
-            quit(1)
-        i += 1
+    tcp_file.close()
 
+    udp_file = open(UDP_FILE_PATH, 'r')
+    # tcp_reader = csv.reader(tcp_file, delimiter=',', quotechar='|')
+    udp_reader = csv.DictReader(udp_file)
 
-    output = open("tcp_flow_durations.csv", "w")
-    tcp_writer = csv.writer(output, delimiter=',',
+    for row in udp_reader:
+
+        a_to_b = row["ip.src"] + ":" + row["udp.srcport"] + " -> " + row["ip.dst"] + ":" + row["udp.dstport"]
+        b_to_a = row["ip.dst"] + ":" + row["udp.dstport"] + " -> " + row["ip.src"] + ":" + row["udp.srcport"]
+
+        is_recorded = a_to_b in udpflows
+        is_inverse_recorded = b_to_a in udpflows
+
+        if not is_recorded and not is_inverse_recorded:
+            udpflows[a_to_b] = [row]
+        elif is_recorded:
+            udpflows[a_to_b].append(row)
+        elif is_inverse_recorded:
+            udpflows[b_to_a].append(row)
+
+    udp_file.close()
+
+    print("Writing to file")
+
+    tcp_output = open("tcp_flow_durations.csv", "w")
+    tcp_writer = csv.writer(tcp_output, delimiter=',',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
     udp_output = open("udp_flow_durations.csv", "w")
@@ -47,24 +62,15 @@ if __name__ == "__main__":
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
     for k, v in tcpflows.items():
-        print(k, v)
-        tcp_writer.writerow([k, v[len(v) - 1].time - v[0].time])
+        duration = 0
+        for p in v:
+            duration += float(p["tcp.time_delta"])
+        tcp_writer.writerow([k, duration])
 
     for k, v in udpflows.items():
-        # print(k, v)
-        udp_writer.writerow([k, v[len(v) - 1].time - v[0].time])
 
-    output.close()
+        udp_writer.writerow([k, float(v[len(v) - 1]["frame.time_epoch"]) - float(v[0]["frame.time_epoch"])])
+
+    tcp_output.close()
     udp_output.close()
 
-    # tcp_output = open("tcp_flow_sizes_packets.csv", "w")
-    # tcp_writer = csv.writer(output, delimiter=',',
-    #                         quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    #
-    # udp_output = open("udp_flow_sizes_packets.csv", "w")
-    # udp_writer = csv.writer(udp_output, delimiter=',',
-    #                         quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    #
-    # for k, v in tcpflows.items():
-    #     print(k, v)
-    #     tcp_writer.writerow([k, len(v)])
